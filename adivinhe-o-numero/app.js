fc0be27bd4859105b88da21a647c7f0c78963c0a
William Soares <pedro.williamsoares2022@gmail.com>
2025-08-07 14:17:16 -0300

    Alteração na quantidade limite de limite para 100

--- adivinhe-o-numero/app.js
+++ adivinhe-o-numero/app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
   exibirNaTela('h1', "Jogo de adivinhar o número secreto");
-  exibirNaTela('p', "Escolha um número entre 1 a 10");
+  exibirNaTela('p', "Escolha um número entre 1 a 100");
 }
 exibirMensagemInicial();
 
